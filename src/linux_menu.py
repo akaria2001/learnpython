c5e9fcddac6dbb2365294b410b3d026ed1d7a08c
amit <amit@rocky8.local>
2021-09-04 19:37:34 +0100

    Added function to build menu

--- src/linux_menu.py
+++ src/linux_menu.py
@@ -24,10 +24,26 @@ def print_blue(text):
     print(f'\033[1;34m{text}\033[0;0m')
 
 
+def generate_menu():
+    menu_items = [[1, 'OS Upgrade', 'sudo yum update -y'],
+                  [2, 'Show Kernel Version', 'uname -r'],
+                  [3, 'Show Disk Space', 'df -h']]
+    return menu_items
+
+
 def main():
     cmd.call("clear", shell=False)
     print_yellow(f"Hello {generate_username()}, welcome to Linux Menu")
     time.sleep(1)
+    while(True):
+        for menu_item in generate_menu():
+            print_green(f"{menu_item[0]}: {menu_item[1]}")
+        print_red("0: Exit Application")
+        user_choice = int(input("Please select option : "))
+        if user_choice == 0:
+            exit()
+        else:
+            next
 
 
 if __name__ == '__main__':
